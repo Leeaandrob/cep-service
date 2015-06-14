cf045e49258dc65ea0975776472037e985310563
Élysson MR <elyssonmr@gmail.com>
2015-06-14 14:31:10 -0300

    Adiciona a interface REST do app CEP

--- cep/models.py
+++ cep/models.py
@@ -8,4 +8,5 @@ class Cep(models.Model):
     logradouro = models.CharField(max_length=150)
     bairro = models.CharField(max_length=100)
     cidade = models.CharField(max_length=100)
+    estado = models.CharField(max_length=100)
     cep = models.CharField(max_length=10)
--- cep/views.py
+++ cep/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+# coding: utf-8
 
-# Create your views here.
+from rest_framework import viewsets
+
+from .models import Cep
+from .serializers import CepSerializer
+
+import logging
+
+logger = logging.getLogger('console.logger')
+
+
+class CepViewSet(viewsets.ModelViewSet):
+    queryset = Cep.objects.all()
+    serializer_class = CepSerializer
+
+    def list(self, request, *args, **kwargs):
+        logger.info('Listagem de Ceps')
+        return super(CepViewSet, self).list(request, *args, **kwargs)
