f5d62f612a0b52fc384160640bc8e0afc1c848bf
Élysson MR <elyssonmr@gmail.com>
2015-06-14 12:38:26 -0300

    adiciona o app cep

---
+++ cep/models.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+from django.db import models
+
+# Create your models here.
+
+
+class Cep(models.Model):
+    logradouro = models.CharField(max_length=150)
+    bairro = models.CharField(max_length=100)
+    cidade = models.CharField(max_length=100)
+    cep = models.CharField(max_length=10)
