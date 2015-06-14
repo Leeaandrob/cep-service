98ef3b5709d88cf27a2606561f4797bd01cc6a95
Élysson MR <elyssonmr@gmail.com>
2015-06-14 14:53:04 -0300

    Adiciona os logs aos verbos do CEP

--- cep/views.py
+++ cep/views.py
@@ -17,3 +17,24 @@ class CepViewSet(viewsets.ModelViewSet):
     def list(self, request, *args, **kwargs):
         logger.info('Listagem de Ceps')
         return super(CepViewSet, self).list(request, *args, **kwargs)
+
+    def retrieve(self, request, *args, **kwargs):
+        obj = self.get_object()
+        logger.info('Recuperando CEP de: %d' % (obj.pk))
+        return super(CepViewSet, self).retrieve(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        logger.info('Criando CEP: %s' % request.data)
+
+        return super(CepViewSet, self).create(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+        obj = self.get_object()
+        logger.info('Atualizando CEP: %d com os dados: %s'
+                    % (obj.pk, request.data))
+        return super(CepViewSet, self).update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        obj = self.get_object()
+        logger.info('Deletando CEP: %d' % obj.pk)
+        return super(CepViewSet, self).destroy(request, *args, **kwargs)
